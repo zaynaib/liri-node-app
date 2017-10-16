2b8135185fe73b83dccf5cfcfac96389b0c0e836
Zaynaib <zaynaibg@gmail.com>
2017-10-16 13:43:41 -0500

    trying to fix spotify-this-song bug

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ How to create a twitter bot
 [https://dzone.com/articles/how-to-use-twitter-api-using-nodejs]
 
 http://www.tutorialsteacher.com/nodejs/nodejs-module-exports
-
+https://github.com/RyanSy/LIRI/blob/master/liri.js
 */
 
 
@@ -53,10 +53,12 @@ var parameter = process.argv[3];
 var parmetersArgs = process.argv;
 var commands = parmetersArgs[2];
 
-console.log(commands);
+//console.log(commands);
 
 function myTweets(){
-	  //create a twitter client to authenicate my twitter account
+	if (commands === "my-tweets") {
+	
+//create a twitter client to authenicate my twitter account
 var client = new Twitter(tweetKeys);
 
 //the person whose tweets you want to search
@@ -67,21 +69,32 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     //console.log(tweets);
     //console.log(response);
-     console.log(JSON.stringify(tweets, null, 2));
+     //console.log(JSON.stringify(tweets, null, 2));
+     for(var i =0 ; i<tweets.length; i++){
+     	console.log(JSON.stringify(tweets[i].text, null, 2));
+     	console.log(JSON.stringify(tweets[i].created_at, null, 2));
+     }
 
   }
 });
+}
 
 }
 
 function mySpotify(parmetersArr){
-	console.log(parmetersArr);
-	var parameter;
+	//console.log(parmetersArr);
+
+	if (commands === "spotify-this-song") {	
+
+	//var parameter;
+	var parameter="";
 	for(var i =3; i<parmetersArr.length;i++){
-		parameter = parmetersArr[i] + "+";
+		//parameter = parmetersArr[i] + "+";
+		parameter = parameter + "+" +parmetersArr[i];
 		console.log(parameter);
 
 	}
+	
 	var spotify = new Spotify(spotifyKeys);
  
 	spotify.search({ type: 'track', query: parameter }, function(err, data) {
@@ -90,20 +103,29 @@ function mySpotify(parmetersArr){
  	 }
  
 	console.log(JSON.stringify(data, null, 2)); 
+	//var myData = JSON.parse(data);
+	//var myData =JSON.stringify(data, null, 2);
+	//console.log(myData.album);
 	});
+	
+	}
+	
 
 }
 
 function movieMagic(parmetersArr){
 
-	var parameter;
+	if (commands === "movie-this") {
+	//console.log(parmetersArgs[3]);
+	
+	var parameter ="";
 	if(parmetersArr[3] === undefined){
 		parameter ="Mr.+Nobody";
 
  	 }else{
 	
 	for(var i =3; i<parmetersArr.length;i++){
-		parameter = parmetersArr[i] + "+";
+		parameter = parameter + " "+ parmetersArr[i];
 		console.log(parameter);
 		}
 	}
@@ -117,38 +139,67 @@ function movieMagic(parmetersArr){
 
     	// Parse the body of the site and recover just the imdbRating
     	// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    	console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    	var title = JSON.parse(body).Title;
+    	var year = JSON.parse(body).Year;
+    	var ratingIMBD = JSON.parse(body).imdbRating;
+    	var ratingRotten = JSON.parse(body).Ratings[1].source;
+       	var country = JSON.parse(body).Country;
+    	var lang = JSON.parse(body).Language;
+    	var moviePlot = JSON.parse(body).Plot;
+    	var movieActors = JSON.parse(body).Actors;
+
+    	console.log("Title of the movie is: " + title);
+    	console.log("Year the movie came out is: " + year);
+    	console.log("IMDB Rating of the movie is: " + ratingIMBD);
+    	console.log("Rotten Tomatoes Rating of the movie is: " + ratingRotten);
+    	console.log("Country where the movie was produced is: " + country);
+    	console.log("Language of the movie is: " + lang);
+    	console.log("Plot of the movie is: " + moviePlot);
+    	console.log("Actors in the movie is: " + movieActors);
+
+
+    	/*
+
+    	  * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie
+
+		*/
+
   		}
 	});
-
+	}
 }
 
 
-if (commands === "my-tweets") {
-	myTweets();
-}
+myTweets();
+mySpotify(parmetersArgs);
+movieMagic(parmetersArgs);
 
 
-if (commands === "spotify-this-song") {
-	//var parmetersArgs = process.argv[];
-	mySpotify(parmetersArgs);
-
-}
 
 
-if (commands === "movie-this") {
-	console.log(parmetersArgs[3]);
-	movieMagic(parmetersArgs);
-}
 
 
 if (commands === "do-what-it-says") {
 
 	try {  
    	 	var textData = fs.readFileSync('random.txt', 'utf8');
-    	//console.log(data);   
+    	console.log(textData);   
     	var dataArr = textData.split(",");
     	console.log(dataArr);
+    	var innerCommands = dataArr[0];
+    	/*
+    	var str = '"a string"';
+		str = str.replace(/^"|"$/g, '');
+		*/
+		
+if (innerCommands === "spotify-this-song") {
 
     var spotify = new Spotify(spotifyKeys);
  
@@ -160,7 +211,35 @@ if (commands === "do-what-it-says") {
 	console.log(JSON.stringify(data, null, 2)); 
 	});
 
-	} catch(e) {
+	}//end of spotify
+
+
+	if (commands === "movie-this") {
+	
+	var parameter =dataArr[1]
+	if(parameter === undefined){
+		parameter ="Mr.+Nobody";
+
+ 	 }
+
+		// Then run a request to the OMDB API with the movie specified
+	request("http://www.omdbapi.com/?t="+parameter+"&y=&plot=short&apikey=40e9cece", 
+	function(error, response, body) {
+
+		// If the request is successful (i.e. if the response status code is 200)
+  		if (!error && response.statusCode === 200) {
+
+    	// Parse the body of the site and recover just the imdbRating
+    	// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    	console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  		}
+	});
+	}//end of movie
+
+
+
+	} //end of try
+	catch(e) {
     	console.log('Error:', e.stack);
 	}
 
